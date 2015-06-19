b445bc8bf3c1236b64e38c8d4bf052dff9f5fd78
Nicolas Siver <nicolas@siver.im>
2015-06-19 21:27:34 +0300

    register hooks

--- plugin.js
+++ plugin.js
@@ -1,3 +1,26 @@
 /**
  * Created by Nicolas on 6/19/15.
  */
+(function (Plugin) {
+    'use strict';
+
+    var translit = require('translitit-cyrillic-russian-to-latin');
+
+    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
+    Plugin.hooks = {
+        filters: {
+            categoryCreate: function (categoryData, callback) {
+
+            },
+
+            topicCreate: function (topicData, callback) {
+
+            },
+
+            userCreate: function (userData, callback) {
+
+            }
+        }
+    };
+
+})(module.exports);
